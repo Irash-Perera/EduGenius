ed164dda6e7c1902ef14b089fca4a94eefd278d1
Irash Perera <irash.21@cse.mrt.ac.lk>
2024-07-06 00:18:57 +0530

    refactor: Adjust container heights for messages and tips in math_solver.py

--- math_solver.py
+++ math_solver.py
@@ -12,10 +12,10 @@ st.subheader(" Let's solve it :red[_step-by-step_]💡", divider= 'red')
 
 col1, col2 = st.columns(2)
 with col1:
-    messages = st.container(height=300)
+    messages = st.container(height=350)
 
 with col2:
-    tips = st.container(height=300)
+    tips = st.container(height=350)
     tips.markdown("""
         <div style="background-color: #0E1117; border-radius: 10px; padding: 20px; height: 300px; box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.1);">
             <h5 style="text-align: left;">Don't get the answer as expected? Here are some tips🤖:</h5>
