73e6eda39c312f874ead793600325416a62852ca
HansanaPrabashwara <129297567+HansanaPrabashwara-210483T@users.noreply.github.com>
2024-09-08 20:57:02 +0530

    Update app.py
    Added the Q&A tab

--- app.py
+++ app.py
@@ -20,13 +20,14 @@ st.logo(sidear_logo, icon_image=main_body_logo)
 
 home_page = st.Page("pages/home.py", title = "Home", icon =":material/home:", default=True)
 dashboard_page = st.Page("pages/dashboard.py", title = "Dashboard", icon =":material/dashboard:")
+QA = st.Page("pages/chat.py", title = "Q&A", icon =":material/question_answer:")
 math_solver = st.Page("pages/math_solver.py", title = "Math Solver", icon =":material/function:")
 # research_helper = st.Page("research_helper.py", title = "Research Helper", icon =":material/insights:")
 contactus_page = st.Page("pages/contact_us.py", title = "Contact Us", icon =":material/call:")
 feedback_page = st.Page("pages/feedbacks.py", title = "Feedback", icon =":material/feedback:")
 documentation_page = st.Page("pages/documentation.py", title = "Documentation", icon =":material/book:")
 pg = st.navigation({
-    "Main Menu":[home_page, dashboard_page, math_solver, contactus_page, feedback_page, documentation_page]
+    "Main Menu":[home_page, dashboard_page,QA, math_solver, contactus_page, feedback_page, documentation_page]
 })
 
 pg.run()
