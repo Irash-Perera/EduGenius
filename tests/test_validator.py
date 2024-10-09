e3555ec73938672584ffa58073b89e4d408b8bec
Pranavan Subendiran <subendiranp@gmail.com>
2024-10-09 16:40:39 +0530

    Update test_validator.py
    Update docstring and comments.

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -73,16 +73,31 @@ valid_email = 'zxc@domain.com'
 
 
 def test_validate_email_length_2(validator):
+    """
+    Functionality: verify the rejection of mail address with length 2.
+    """
+    # verifying whether mail_id is of length of 2
     assert len(invalid_length_2) == 2
 
+    # verify whether the mail_id with length 2 is successfully rejected.
     assert validator.validate_email(invalid_length_2) == False
 
+
 def test_validate_email_length_320(validator):
+    """
+    Functionality: verify the rejection of mail address with length 320.
+    """
+    # verifying whether the mail_id is of length of 320
     assert len(invalid_length_320) == 320
 
+    # verify whether the mail_id with length 320 is successfully rejected.
     assert validator.validate_email(invalid_length_320) == False
 
+
 def test_validate_email_without_at(validator):
+    """
+    Functionality: verify the rejection of mail address without '@' symbol.
+    """
     # verifyig the length constraint
     assert 2 < len(invalid_mail_without_at) < 320
 
@@ -92,12 +107,16 @@ def test_validate_email_without_at(validator):
     # verifying whether the mail_id is rejected
     assert validator.validate_email(invalid_mail_without_at) == False
 
+
 def test_validate_email_valid_mail(validator):
+    """
+    Functionality: verify the acceptance of mail address with length between 2 and 320 and having '@' symbol in it.
+    """
     # verifyig the length constraint
     assert 2 < len(valid_email) < 320
 
-    # verifying @ is not present
+    # verifying @ is present
     assert '@' in valid_email
 
-    # verifying whether the mail_id is rejected
+    # verifying whether the mail_id is acctected
     assert validator.validate_email(valid_email) == True
