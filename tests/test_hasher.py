1846e1a2b7b9c8721b98137bb912aa5f42b63df5
Pranavan Subendiran <subendiranp@gmail.com>
2024-10-09 15:02:44 +0530

    Update test_hasher.py
    update test_generate method test case

--- tests/test_hasher.py
+++ tests/test_hasher.py
@@ -7,6 +7,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from auth.hasher import Hasher
 
+
 # mock password list for testing.
 passwords = ['12345', 'abcdef', '%##@zxcvbn9087']
 
@@ -47,3 +48,28 @@ def test_hash(hasher):
     # we used .encode() methods to convert string into string.
     assert bcrypt.checkpw(password=password.encode(), hashed_password=hashed_pw.encode())
 
+
+def test_generate(hasher):
+    """
+    Functionality: verifies the functionality of generate method of Hasher under hasher.py
+    """
+    hashed_pws = hasher.generate()
+
+    # verify the type of the original password 
+    assert type(passwords) == list
+
+    # verify the length of passwords are equal
+    assert len(passwords) == len(hashed_pws)
+
+    
+    
+    for h_pw, pw in zip(hashed_pws, passwords):
+        # verify the type of each hashed password as string.
+        assert type(h_pw) == str
+    
+        # verify hashed password is different from original password.
+        assert h_pw != pw
+
+        # compare hashed password and original password 
+        # 'to verify whether the hashed original password is the actual hased version of it
+        assert bcrypt.checkpw(hashed_password=h_pw.encode(), password=pw.encode())
