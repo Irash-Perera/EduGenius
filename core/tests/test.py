0b8aea5e83fd8e7ed3a90ee8258a8f741031372d
HansanaPrabashwara <129297567+HansanaPrabashwara-210483T@users.noreply.github.com>
2024-10-09 17:59:35 +0530

    Update test.py

--- core/tests/test.py
+++ core/tests/test.py
@@ -1,23 +1,28 @@
-from metrics import RAG_Tester
+from metrics import RAG_Tester, RAG_Report
+import pandas as pd
 
-test_questions = ["What is the Life Insurance Corporation of India (LIC) known for?"]
-test_ground_truth =  ["The Life Insurance Corporation of India (LIC) is the largest insurance company in India, established in 1956 through the nationalization of the insurance industry. It is known for managing a large portfolio of investments."]
-test_answer = ["The Life Insurance Corporation of India (LIC) is the largest insurance company in India, known for its vast portfolio of investments. LIC contributs to the financial stability of the country."]
-test_contexts = [[
-        "The Life Insurance Corporation of India (LIC) was established in 1956 following the nationalization of the insurance industry in India.",
-        "LIC is the largest insurance company in India, with a vast network of policyholders and a huge investments.",
-        "As the largest institutional investor in India, LIC manages a substantial funds, contributing to the financial stability of the country.",
-        "The Indian economy is one of the fastest-growing major economies in the world, thanks to the secors like finance, technology, manufacturing etc"
-    ]]
 
+df = pd.read_excel("./test_data/TestCases.xlsx",index_col=False)
+print()
+
+test_questions = df["Question"].to_list()
+test_ground_truth =  df["Ground_Truth"].to_list()
+test_answer = df["Answer"].to_list()
+test_contexts = []
+
+for i in range(len(df["Context"].to_list())):
+    test_contexts.append([df["Context"].to_list()[i]])
 
 rag_tester = RAG_Tester(test_questions, test_answer, test_contexts, test_ground_truth)
+# rag_report = RAG_Report(test_questions, test_answer, test_contexts, test_ground_truth)
 
-# print(rag_tester.faithfulness())
-# print(rag_tester.answerRelavance())
-# print(rag_tester.contextPrecision())
-# print(rag_tester.contextRecall())
-# print(rag_tester.contextEntitiesRecall())
-# print(rag_tester.contextUtilization())
-# print(rag_tester.noiseSensitivity())
+print(rag_tester.faithfulness())
+print(rag_tester.answerRelavance())
+print(rag_tester.contextPrecision())
+print(rag_tester.contextRecall())
+print(rag_tester.contextEntitiesRecall())
+print(rag_tester.contextUtilization())
+print(rag_tester.noiseSensitivity())
 print(rag_tester.aspectCritique("correctness", "Is the submission factually correct?"))
+
+# print(rag_report.generate_report())
