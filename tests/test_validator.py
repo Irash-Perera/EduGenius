b67d0c5527900fd3d09d23e422e9355569994368
Pranavan Subendiran <subendiranp@gmail.com>
2024-10-09 17:04:18 +0530

    Update test_validator.py
    added cases for validating names

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -120,3 +120,68 @@ def test_validate_email_valid_mail(validator):
 
     # verifying whether the mail_id is acctected
     assert validator.validate_email(valid_email) == True
+
+
+###########################################################################################
+"""
+Testing the validate_name method of Validator object.
+"""    
+
+# mock testing values for names.
+invalid_name_length_1='e'
+invalid_name_length_100='eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+invalid_name_with_wrong_type = 1110  # not a string value(integer value)
+valid_name = 'user123'
+
+def test_validate_name_length_1(validator):
+    """
+    Functionality: verify the rejection of name with length 1
+    """
+    # verifying length of the name is 1
+    assert len(invalid_name_length_1)==1
+
+    # verify name is of string type
+    assert type(invalid_name_length_1)==str
+
+    # verify whether the name with length 1 is successfully rejected.
+    assert validator.validate_name(invalid_name_length_1) == False
+
+
+def test_validate_name_length_100(validator):
+    """
+    Functionality: verify the rejection of name with length 100
+    """
+    # verifying length of the name is 100
+    assert len(invalid_name_length_100)==100
+
+    # verify name is of string type
+    assert type(invalid_name_length_100)==str
+
+    # verify whether the name with length 100 is successfully rejected.
+    assert validator.validate_name(invalid_name_length_100) == False
+
+
+def test_validate_name_of_wrong_type(validator):
+    """
+    Functionality: verify the rejection of name of non-string type
+    """
+    # verify name is not of string type
+    assert type(invalid_name_with_wrong_type) != str
+
+    # verify whether the name with wrong type causes type error
+    with pytest.raises(TypeError):
+        validator.validate_name(invalid_name_with_wrong_type)
+
+def test_validate_name_valid_name(validator):
+    """
+    Functionality: verify the acceptance of the correct name
+    """
+    # verifying length of the name is 100
+    assert 1 < len(valid_name) < 100
+
+    # verify name is of string type
+    assert type(valid_name)==str
+
+    # verify whether the name is successfully accepted.
+    assert validator.validate_name(valid_name) == True
+    
