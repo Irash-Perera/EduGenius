48e6808c9e42067920bd6a2e40d09c6969c79186
Pranavan Subendiran <subendiranp@gmail.com>
2024-10-10 13:26:49 +0530

    Update test_authenticate.py
    Fixed the mocking the collection issue.

--- tests/test_authenticate.py
+++ tests/test_authenticate.py
@@ -6,17 +6,16 @@ import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from auth.authenticate import Authenticate  
-@pytest.fixture
-def mock_collection():
-    return Collection(database='database', name='abc')
+from auth.authenticate import Authenticate
+from auth.db import collection
+
 
 @pytest.fixture
-def authenticate(mock_collection):
-    return Authenticate(mock_collection, 'cookie_name', 'key', cookie_expiry_days=30.0)
+def authenticate():
+    return Authenticate(collection, 'cookie_name', 'key', cookie_expiry_days=30.0)
 
 def test_init(authenticate):
-    assert authenticate.collection == mock_collection
+    assert authenticate.collection == collection
     assert authenticate.cookie_name == 'cookie_name'
     assert authenticate.key == 'key'
     assert authenticate.cookie_expiry_days == 30.0
