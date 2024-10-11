ffa11556629fc3fb0fb62358c2e41cd953f70e86
HansanaPrabashwara <129297567+HansanaPrabashwara-210483T@users.noreply.github.com>
2024-10-11 17:10:02 +0530

    Update qa_chat.py

--- core/qa_chat.py
+++ core/qa_chat.py
@@ -1,18 +1,27 @@
 import google.generativeai as genai
 from langchain.prompts import PromptTemplate
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import streamlit as st
 from dotenv import load_dotenv
 import os
 from langchain.vectorstores import Chroma
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
+import sys
+
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from core.wolfarm import get_wolframalpha_response
+
 from utils.createVDB.create_db import embeddings
 
-from pages.math_solver import get_wolframalpha_response
 
 load_dotenv()
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+
 
 
 def reformat_curly_brackets(text):
@@ -73,39 +82,22 @@ def convert_to_markdown(response):
 
 
 
-def respond_for_user_question(user_question,llm):
-    """Generate the platform response for the user queries
-
-    Args:
-        user_question (string): User's query
-        llm (langchain.models.LLM): LLM model to be used to generate the answer  
-
-    Returns:
-        string : generated response for the user query
-    """
-
-
+def QA_RAG(query,llm,vs1_path,vs2_path,k,threshold,session = None):
 
     # Textbook Vector Store data rerical
-    vector_textbook = Chroma(persist_directory='vectorstore_text_books', embedding_function=embeddings)
+    vector_textbook = Chroma(persist_directory=vs1_path, embedding_function=embeddings)
     textbook_retriver = vector_textbook.as_retriever(
         search_type="similarity_score_threshold",
-        search_kwargs={'score_threshold': 0.4}
+        search_kwargs={'score_threshold': threshold}
     )
-    relevent_textbook_documents = textbook_retriver.get_relevant_documents(user_question)
+    relevent_textbook_documents = textbook_retriver.get_relevant_documents(query)
     relevent_textbook_content = get_page_content(relevent_textbook_documents)
-    
+
     # Questions vectorstore data retival chain
-    vectordb = Chroma(persist_directory='vectorstore_2018_OL', embedding_function=embeddings)
-    retriever = vectordb.as_retriever(search_kwargs={"k": 2})
+    vectordb = Chroma(persist_directory=vs1_path, embedding_function=embeddings)
+    retriever = vectordb.as_retriever(search_kwargs={"k": k})
 
     # Get session data
-    
-    ## Chat History
-    history = "history: "
-    for message in st.session_state.messages:
-        history += reformat_curly_brackets(message["content"]) + " "
-
     question_str = ''
     answer_str = ''
     explanations_str = ''
@@ -113,48 +105,55 @@ def respond_for_user_question(user_question,llm):
     similar_problems_str = ''
     improvements_str = ''
     hints_str = ''
-
-    question = st.session_state["question_text"]
-    answer = st.session_state["answer"]
-    explanations = st.session_state["explanation"]
-    marks = st.session_state["marks"]
-    similar_problems = st.session_state["similar_problems"]
-    improvements = st.session_state["improvement"]
-    hints = st.session_state["hints"]
+    history = ''
     
-    if question != None:
-        question_str = f"question : {reformat_curly_brackets(question)}"
+    ## Chat History
+    if session != None:
+        history = "history: "
+        for message in session["messages"]:
+            history += reformat_curly_brackets(message["content"]) + " "
 
-    if answer != None:
-        answer_str = f"provided answer : {reformat_curly_brackets(answer)}"
+        question = session["question_text"]
+        answer = session["answer"]
+        explanations = session["explanation"]
+        marks = session["marks"]
+        similar_problems = session["similar_problems"]
+        improvements = session["improvement"]
+        hints = session["hints"]
+        
+        if question != None:
+            question_str = f"question : {reformat_curly_brackets(question)}"
 
-    if explanations != None:
-        explanations_str = f"explanation: {reformat_curly_brackets(explanations)}"
-    
-    if marks != None:
-        marks_str = f"marks : {reformat_curly_brackets(marks)}"
-                
-    if similar_problems != None:
-        similar_problems_str = f"similar problems : "
-        for i in similar_problems:
-            similar_problems_str += reformat_curly_brackets(i)
-            similar_problems += ", "
+        if answer != None:
+            answer_str = f"provided answer : {reformat_curly_brackets(answer)}"
 
-    if improvements != None:
-        improvements_str = f"improvements: {reformat_curly_brackets(improvements)}"       
+        if explanations != None:
+            explanations_str = f"explanation: {reformat_curly_brackets(explanations)}"
+        
+        if marks != None:
+            marks_str = f"marks : {reformat_curly_brackets(marks)}"
+                    
+        if similar_problems != None:
+            similar_problems_str = f"similar problems : "
+            for i in similar_problems:
+                similar_problems_str += reformat_curly_brackets(i)
+                similar_problems += ", "
 
-    if hints != None:
-        hints_str = f"hints: {hints}"   
-        for i in hints:
-            hints_str += reformat_curly_brackets(i) 
-            hints += ", "   
+        if improvements != None:
+            improvements_str = f"improvements: {reformat_curly_brackets(improvements)}"       
+
+        if hints != None:
+            hints_str = f"hints: {hints}"   
+            for i in hints:
+                hints_str += reformat_curly_brackets(i) 
+                hints += ", "   
 
 
     wolfram_text = ""
-    if question == None:
-        wolfram_text = get_wolframalpha_response(user_question)
-        # print(wolfram_text)
-        wolfram_text = convert_to_markdown(wolfram_text)
+    # if question == None:
+    #     wolfram_text = get_wolframalpha_response(user_question)
+    #     # print(wolfram_text)
+    #     wolfram_text = convert_to_markdown(wolfram_text)
 
 
     # Create the retrieval chain
@@ -178,31 +177,164 @@ def respond_for_user_question(user_question,llm):
     potential_answer:"""+ wolfram_text+"""
     context: {context}
 
-
+    
     input: {input}
     answer:
     """
     template = template2
-    if question != None:
-        template = template1
+    if session != None:
+        if question != None:
+            template = template1
 
-    formatted_user_question = reformat_curly_brackets(user_question)
+    formatted_user_question = reformat_curly_brackets(query)
     prompt = PromptTemplate.from_template(template)
     combine_docs_chain = create_stuff_documents_chain(llm, prompt)
     retrieval_chain = create_retrieval_chain(retriever, combine_docs_chain)
     response=retrieval_chain.invoke({"input":formatted_user_question})
-    st.session_state.messages.append({"role": "assistant", "content": response["answer"]})
-    # print("Question -----------------------------------------")
-    # print(formatted_user_question)
-    # print("Answer -------------------------------------------")
-    # print(response["answer"])
-    # print("Context ------------------------------------------")
-    # print(response["context"])
-    # print("template -----------------------------------------")
-    # print(template)
 
+    return response
+
+
+
+def respond_for_user_question(user_question,llm):
+    response = QA_RAG(user_question, llm,"vectorstore_text_books","vectorstore_2018_OL", 2, 0.4, dict(st.session_state))
+    st.session_state.messages.append({"role": "assistant", "content": response["answer"]})
 
     with st.chat_message("assistant"):
         st.write(response["answer"])
 
-    return response
+
+
+
+# def respond_for_user_question(user_question,llm):
+#     """Generate the platform response for the user queries
+
+#     Args:
+#         user_question (string): User's query
+#         llm (langchain.models.LLM): LLM model to be used to generate the answer  
+
+#     Returns:
+#         string : generated response for the user query
+#     """
+
+
+
+#     # Textbook Vector Store data rerical
+#     vector_textbook = Chroma(persist_directory='vectorstore_text_books', embedding_function=embeddings)
+#     textbook_retriver = vector_textbook.as_retriever(
+#         search_type="similarity_score_threshold",
+#         search_kwargs={'score_threshold': 0.4}
+#     )
+#     relevent_textbook_documents = textbook_retriver.get_relevant_documents(user_question)
+#     relevent_textbook_content = get_page_content(relevent_textbook_documents)
+    
+#     # Questions vectorstore data retival chain
+#     vectordb = Chroma(persist_directory='vectorstore_2018_OL', embedding_function=embeddings)
+#     retriever = vectordb.as_retriever(search_kwargs={"k": 2})
+
+#     # Get session data
+    
+#     ## Chat History
+#     history = "history: "
+#     for message in st.session_state.messages:
+#         history += reformat_curly_brackets(message["content"]) + " "
+
+#     question_str = ''
+#     answer_str = ''
+#     explanations_str = ''
+#     marks_str = ''
+#     similar_problems_str = ''
+#     improvements_str = ''
+#     hints_str = ''
+
+#     question = st.session_state["question_text"]
+#     answer = st.session_state["answer"]
+#     explanations = st.session_state["explanation"]
+#     marks = st.session_state["marks"]
+#     similar_problems = st.session_state["similar_problems"]
+#     improvements = st.session_state["improvement"]
+#     hints = st.session_state["hints"]
+    
+#     if question != None:
+#         question_str = f"question : {reformat_curly_brackets(question)}"
+
+#     if answer != None:
+#         answer_str = f"provided answer : {reformat_curly_brackets(answer)}"
+
+#     if explanations != None:
+#         explanations_str = f"explanation: {reformat_curly_brackets(explanations)}"
+    
+#     if marks != None:
+#         marks_str = f"marks : {reformat_curly_brackets(marks)}"
+                
+#     if similar_problems != None:
+#         similar_problems_str = f"similar problems : "
+#         for i in similar_problems:
+#             similar_problems_str += reformat_curly_brackets(i)
+#             similar_problems += ", "
+
+#     if improvements != None:
+#         improvements_str = f"improvements: {reformat_curly_brackets(improvements)}"       
+
+#     if hints != None:
+#         hints_str = f"hints: {hints}"   
+#         for i in hints:
+#             hints_str += reformat_curly_brackets(i) 
+#             hints += ", "   
+
+
+#     wolfram_text = ""
+#     # if question == None:
+#     #     wolfram_text = get_wolframalpha_response(user_question)
+#     #     # print(wolfram_text)
+#     #     wolfram_text = convert_to_markdown(wolfram_text)
+
+
+#     # Create the retrieval chain
+#     template1 = """
+#     You are a helpful AI math tutor.
+#     Answer based on the following data provided.
+#     \n"""+ question_str +'\n'+ answer_str +'\n'+ explanations_str +'\n'+ marks_str +'\n'+ improvements_str +'\n'+ history  +'\n'+ similar_problems_str +'\n'+ hints_str+'\n'+"""
+    
+#     textbook content:""" +relevent_textbook_content+ """
+    
+#     context: {context}
+#     input: {input}
+#     answer:
+#     """
+
+#     template2 = """
+#     You are a helpful AI math tutor.
+#     try to answer the user query. Try to follow the steps provided in the context.Ignore the context.
+#     Answer in friendly, explaining manner.
+#     textbook content:""" +relevent_textbook_content+ """
+#     potential_answer:"""+ wolfram_text+"""
+#     context: {context}
+
+
+#     input: {input}
+#     answer:
+#     """
+#     template = template2
+#     if question != None:
+#         template = template1
+
+#     formatted_user_question = reformat_curly_brackets(user_question)
+#     prompt = PromptTemplate.from_template(template)
+#     combine_docs_chain = create_stuff_documents_chain(llm, prompt)
+#     retrieval_chain = create_retrieval_chain(retriever, combine_docs_chain)
+#     response=retrieval_chain.invoke({"input":formatted_user_question})
+#     # print("Question -----------------------------------------")
+#     # print(formatted_user_question)
+#     # print("Answer -------------------------------------------")
+#     # print(response["answer"])
+#     # print("Context ------------------------------------------")
+#     # print(response["context"])
+#     # print("template -----------------------------------------")
+#     # print(template)
+
+
+#     with st.chat_message("assistant"):
+#         st.write(response["answer"])
+
+#     return response
