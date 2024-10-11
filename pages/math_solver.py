2d4737e9d96956f27c59492505e99ae8e9f463c0
HansanaPrabashwara <129297567+HansanaPrabashwara-210483T@users.noreply.github.com>
2024-10-11 08:54:32 +0530

    Update math_solver.py

--- pages/math_solver.py
+++ pages/math_solver.py
@@ -8,6 +8,20 @@ from dotenv import load_dotenv
 from langfuse import Langfuse
 from langfuse.decorators import observe, langfuse_context
 
+from auth.authenticate import Authenticate
+from auth.db import collection
+import yaml
+
+with open('config.yaml') as file:
+    config = yaml.load(file, Loader=yaml.SafeLoader)
+
+authenticator = Authenticate(
+    collection,
+    config['cookie']['name'], 
+    config['cookie']['key'], 
+    config['cookie']['expiry_days'],
+)
+
 load_dotenv()
 
  
